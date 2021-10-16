0fb60a95c29796524f3da71929bbc94a97850276
Christian Prada <christian.prada.mt@correo.usa.edu.co>
2021-10-16 10:36:12 -0500

    MVC Client implementado y funcionando

--- Reto3/src/main/java/co/usa/reto3/model/Client.java
+++ Reto3/src/main/java/co/usa/reto3/model/Client.java
@@ -1,4 +1,47 @@
 package co.usa.reto3.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "client")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCli;
+    private String nameCli;
+    private String emailCli;
+    private Integer ageCli;
+
+    public Integer getIdCli() {
+        return idCli;
+    }
+
+    public void setIdCli(Integer idCli) {
+        this.idCli = idCli;
+    }
+
+    public String getNameCli() {
+        return nameCli;
+    }
+
+    public void setNameCli(String nameCli) {
+        this.nameCli = nameCli;
+    }
+
+    public String getEmailCli() {
+        return emailCli;
+    }
+
+    public void setEmailCli(String emailCli) {
+        this.emailCli = emailCli;
+    }
+
+    public Integer getAgeCli() {
+        return ageCli;
+    }
+
+    public void setAgeCli(Integer ageCli) {
+        this.ageCli = ageCli;
+    }
 }
---
+++ Reto3/src/main/java/co/usa/reto3/web/ClientController.java
@@ -0,0 +1,35 @@
+package co.usa.reto3.web;
+
+import co.usa.reto3.model.Client;
+import co.usa.reto3.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/client")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping("/all")
+    public List<Client> getClients() {
+        return clientService.getAll();
+    }
+
+    @GetMapping("/{idCli}")
+    public Optional<Client> getClientById(@PathVariable("idCli") int idCli) {
+        return clientService.getClient(idCli);
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client save(@RequestBody Client client) {
+        return clientService.save(client);
+    }
+}
